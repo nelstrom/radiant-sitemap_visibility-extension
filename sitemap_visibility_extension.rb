39b4150f786e26b58821202fb5911de541cdec37
Andrew Neil <andrew.jr.neil@gmail.com>
2009-06-06 00:23:16 +0100

    Added "Hide from sitemap" checkbox to page form.

--- sitemap_visibility_extension.rb
+++ sitemap_visibility_extension.rb
@@ -1,23 +1,14 @@
-# Uncomment this if you reference any of your controllers in activate
-# require_dependency 'application'
-
 class SitemapVisibilityExtension < Radiant::Extension
-  version "1.0"
-  description "Describe your extension here"
-  url "http://yourwebsite.com/sitemap_visibility"
-  
-  # define_routes do |map|
-  #   map.namespace :admin, :member => { :remove => :get } do |admin|
-  #     admin.resources :sitemap_visibility
-  #   end
-  # end
+  version "0.1"
+  description "Adds a 'hide from sitemap' flag to pages."
+  url "http://yourwebsite.com/no_map_flag"
   
   def activate
-    # admin.tabs.add "Sitemap Visibility", "/admin/sitemap_visibility", :after => "Layouts", :visibility => [:all]
+    Page.send :include, SitemapVisibilityTags
+    admin.page.edit.add :parts_bottom, "hide_from_sitemap_checkbox", :after => "edit_layout_and_type"
   end
   
   def deactivate
-    # admin.tabs.remove "Sitemap Visibility"
   end
   
 end
