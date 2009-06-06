3e25da2f3e31a2c43932beca3303afdf97f8386c
Andrew Neil <andrew.jr.neil@gmail.com>
2009-06-06 01:14:11 +0100

    Migration now sets default hide_from_sitemap = 0.

--- db/migrate/20090605213628_add_hide_from_sitemap_to_pages.rb
+++ db/migrate/20090605213628_add_hide_from_sitemap_to_pages.rb
@@ -1,6 +1,6 @@
 class AddHideFromSitemapToPages < ActiveRecord::Migration
   def self.up
-    add_column :pages, :hide_from_sitemap, :boolean
+    add_column :pages, :hide_from_sitemap, :boolean, :default => 0
   end
 
   def self.down
